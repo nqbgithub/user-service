19370189617e5fc3f7f4df2a989e6f54fb16ca32
nqb <qibao.nan@topmdrt.com>
2018-04-27 15:50:45 +0800

    update

---
+++ src/main/java/com/strial/staffe/userservice/UserServiceApplication.java
@@ -0,0 +1,16 @@
+package com.strial.staffe.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 客户端服务
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class UserServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+}
---
+++ src/main/java/com/strial/staffe/userservice/controller/UserControlller.java
@@ -0,0 +1,23 @@
+package com.strial.staffe.userservice.controller;
+
+import com.netflix.discovery.converters.Auto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户服务控制器
+ *
+ * @author nanqibao
+ */
+@RestController
+public class UserControlller {
+    @Autowired
+    private Environment environment;
+
+    @RequestMapping("/sysinfo")
+    public String getSysInfo(){
+        return "获取系统消息" + environment.getProperty("driver");
+    }
+}
