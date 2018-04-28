d7ca0ce0f0073cdda006e58082c8b03c47d814be
nqb13510951690 <nqb13510951690@sina.com>
2018-04-28 14:13:47 +0800

    update

--- src/main/java/com/strial/staffe/userservice/UserServiceApplication.java
+++ src/main/java/com/strial/staffe/userservice/UserServiceApplication.java
@@ -3,12 +3,17 @@ package com.strial.staffe.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * 客户端服务
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix
 public class UserServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
--- src/main/java/com/strial/staffe/userservice/controller/UserControlller.java
+++ src/main/java/com/strial/staffe/userservice/controller/UserControlller.java
@@ -1,7 +1,6 @@
 package com.strial.staffe.userservice.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,11 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class UserControlller {
-    @Autowired
-    private Environment environment;
 
-    @RequestMapping("/sysinfo")
+    @Value("${driver}")
+    private String driver;
+
+    @Value("${name}")
+    private String name;
+
+    @RequestMapping("/sms/db_info")
     public String getSysInfo(){
-        return "获取系统消息" + environment.getProperty("driver");
+        return "连接数据库的信息："+this.driver+","+this.name;
     }
 }
